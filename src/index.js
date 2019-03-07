f1d1f9afd07d277117ed343837e7ad51e7b3a63d
Jamin Thornsberry <jthornsberry@aspirehealthcare.com>
2019-03-06 20:35:46 -0600

    Initial implementation

---
+++ src/index.js
@@ -0,0 +1,158 @@
+import pathToRegexp from 'path-to-regexp'
+import queryString from 'query-string'
+
+// Action Types
+export const NAVIGATE = '@@reduxRouter/NAVIGATE'
+const PUSH = '@@reduxRouter/PUSH'
+const REPLACE = '@@reduxRouter/REPLACE'
+const OPEN = '@@reduxRouter/OPEN'
+const GO = '@@reduxRouter/GO'
+const GO_BACK = '@@reduxRouter/GO_BACK'
+const GO_FORWARD = '@@reduxRouter/GO_FORWARD'
+
+// Action Creators
+const navigate = (route, params) => ({
+  type: NAVIGATE,
+  payload: { route, params },
+})
+
+export const push = (route, params = {}) => ({
+  type: PUSH,
+  payload: { route, params },
+})
+
+export const replace = (route, params = {}) => ({
+  type: REPLACE,
+  payload: { route, params },
+})
+
+export const open = (route, params = {}) => ({
+  type: OPEN,
+  payload: { route, params },
+})
+
+export const go = offset => ({
+  type: GO,
+  payload: offset,
+})
+
+export const goBack = () => ({
+  type: GO_BACK,
+})
+
+export const goForward = () => ({
+  type: GO_FORWARD,
+})
+
+// Router Configuration
+const createInstance = (constructor, properties) => {
+  const instance = Object.create(constructor.prototype)
+  return Object.assign(instance, properties)
+}
+
+export const Route = (name, path) => createInstance(Route, { name, path })
+
+export const Fallback = name => createInstance(Fallback, { name })
+
+export const Redirect = (from, to) => createInstance(Redirect, { from, to })
+
+export const Scope = (base, router) => createInstance(Scope, { base, router })
+
+export const Router = routes => createInstance(Router, { routes })
+
+// Middleware
+const getPathParamNames = path =>
+  pathToRegexp
+    .parse(path)
+    .filter(token => token instanceof Object)
+    .map(token => token.name)
+
+const keyFilter = (object, condition) =>
+  Object.entries(object).reduce((params, [key, val]) => {
+    if (condition(key)) params[key] = val
+    return params
+  }, {})
+
+const routeToLocation = (router, name, params) => {
+  const route = router.routes.find(route => route.name === name)
+
+  if (!route) {
+    throw Error('No route found')
+  }
+
+  const pathParamNames = getPathParamNames(route.path)
+  const pathParams = keyFilter(params, key => pathParamNames.includes(key))
+  const queryParams = keyFilter(params, key => !pathParamNames.includes(key))
+  const pathname = pathToRegexp.compile(route.path)(pathParams)
+  const search = queryString.stringify(queryParams)
+
+  return { pathname, search }
+}
+
+const locationToRoute = (router, location) => {
+  const match = route => pathToRegexp(route.path).test(location.pathname)
+  const route = router.routes.find(match)
+
+  if (!route) {
+    throw Error('No route found')
+  }
+
+  const pattern = pathToRegexp(route.path)
+  const pathParamNames = getPathParamNames(route.path)
+  const pathParamValues = pattern.exec(location.pathname).slice(1)
+  const pathParams = pathParamNames.reduce((params, name, index) => {
+    const value = pathParamValues[index]
+    if (value !== undefined) params[name] = value
+    return params
+  }, {})
+  const queryParams = queryString.parse(location.search)
+  const params = { ...pathParams, ...queryParams }
+
+  return { route, params }
+}
+
+const isAbsoluteAction = ({ type }) => [PUSH, REPLACE, OPEN].includes(type)
+
+const isRelativeAction = ({ type }) => [GO, GO_BACK, GO_FORWARD].includes(type)
+
+export const createMiddleware = (router, history) => store => {
+  history.listen(location => {
+    const { route, params } = locationToRoute(router, location)
+    const action = navigate(route.name, params)
+
+    store.dispatch(action)
+  })
+
+  return next => action => {
+    if (isAbsoluteAction(action)) {
+      const { route, params } = action.payload
+      const location = routeToLocation(router, route, params)
+
+      switch (action.type) {
+        case PUSH:
+          history.push(location)
+          break
+        case REPLACE:
+          history.replace(location)
+          break
+        case OPEN:
+          window.open(location)
+          break
+      }
+    } else if (isRelativeAction(action)) {
+      switch (action.type) {
+        case GO:
+          history.go(action.payload)
+          break
+        case GO_BACK:
+          history.goBack()
+          break
+        case GO_FORWARD:
+          history.goForward()
+          break
+      }
+    } else {
+      return next(action)
+    }
+  }
+}
---
+++ src/index.spec.js
@@ -0,0 +1,83 @@
+import { createMemoryHistory } from 'history'
+import configureStore from 'redux-mock-store'
+import {
+  Fallback,
+  NAVIGATE,
+  Redirect,
+  Route,
+  Router,
+  Scope,
+  createMiddleware,
+  replace,
+} from './index'
+
+const userRouter = Router([
+  Route('users', '/'),
+  Route('user', '/:userId'),
+  Route('userAdmin', '/admin'),
+])
+
+const router = Router([
+  Route('home', '/'),
+  Route('cart', '/cart'),
+  Route('search', '/search/:category?'),
+  Route('item', '/item/:itemId'),
+  Redirect('/product/:itemId', 'item'),
+  Scope('/users', userRouter),
+  Fallback('notFound'),
+])
+
+const mocks = (initialEntries = []) => {
+  const history = createMemoryHistory({ initialEntries })
+  const middleware = createMiddleware(router, history)
+  const store = configureStore([middleware])()
+
+  return { store, history }
+}
+
+const CHANGING_LOCATION_TESTS = [
+  ['/', 'home', {}],
+  ['/cart', 'cart', {}],
+  ['/search', 'search', {}],
+  ['/search/widgets', 'search', { category: 'widgets' }],
+  ['/item/123', 'item', { itemId: '123' }],
+  ['/product/123', 'item', { itemId: '123' }],
+  ['/users', 'users', {}],
+  ['/users/456', 'user', { userId: '456' }],
+  ['/users/admin', 'userAdmin', {}],
+  ['/nonsense', 'notFound', {}],
+]
+
+describe('changing location', () => {
+  CHANGING_LOCATION_TESTS.forEach(([path, route, params]) => {
+    test(`dispatches correct action when changed to '${path}'`, () => {
+      const { store, history } = mocks()
+      const action = { type: NAVIGATE, payload: { route, params } }
+
+      history.replace(path)
+      expect(store.getActions()).toEqual([action])
+    })
+  })
+})
+
+const DISPATCHING_ACTION_TESTS = [
+  ['home', undefined, '/'],
+  ['cart', undefined, '/cart'],
+  ['search', undefined, '/search'],
+  ['search', { category: 'widgets' }, '/search/widgets'],
+  ['item', { itemId: '123' }, '/item/123'],
+  ['users', undefined, '/users'],
+  ['user', { userId: '456' }, '/users/456'],
+  ['userAdmin', undefined, '/users/admin'],
+]
+
+describe('dispatching actions', () => {
+  DISPATCHING_ACTION_TESTS.forEach(([route, params, path]) => {
+    test(`changes location to '${path}'`, () => {
+      const { store, history } = mocks()
+
+      store.dispatch(replace(route, params))
+      expect(history.location.pathname + history.location.search).toBe(path)
+    })
+  })
+})
