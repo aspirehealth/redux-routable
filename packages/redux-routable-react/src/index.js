9e397971d76dbc21c8749fad7e50de9bd61609f4
Jamin Thornsberry <jthornsberry@aspirehealthcare.com>
2019-03-22 10:41:11 -0500

    Dispatch actions when clicking a Link

--- packages/redux-routable-react/src/index.js
+++ packages/redux-routable-react/src/index.js
@@ -1,7 +1,8 @@
 import { createPath } from 'history'
 import PropTypes from 'prop-types'
-import React, { createContext, useContext } from 'react'
-import { Router, routeToLocation } from 'redux-routable'
+import React, { createContext, useCallback, useContext } from 'react'
+import { connect } from 'react-redux'
+import { Router, open, push, replace, routeToLocation } from 'redux-routable'
 
 const RouterContext = createContext()
 
@@ -16,20 +17,32 @@ RouterProvider.propTypes = {
 }
 
 // Link
-export const Link = ({
-  component: Component,
-  action,
-  route,
-  params,
-  hash,
-  ...props
-}) => {
-  const router = useContext(RouterContext)
-  const location = routeToLocation(router, route, params, hash)
-  const href = createPath(location)
+const linkActionCreators = { push, replace, open }
 
-  return <Component {...props} href={href} />
-}
+export const Link = connect()(
+  ({
+    component: Component,
+    action,
+    route,
+    params,
+    hash,
+    dispatch,
+    ...props
+  }) => {
+    const router = useContext(RouterContext)
+    const location = routeToLocation(router, route, params, hash)
+    const href = createPath(location)
+
+    const onClick = useCallback(() => {
+      const actionCreator = linkActionCreators[action]
+      const linkAction = actionCreator(route, params, hash)
+
+      dispatch(linkAction)
+    }, [action, href])
+
+    return <Component {...props} href={href} onClick={onClick} />
+  },
+)
 
 Link.propTypes = {
   component: PropTypes.oneOfType([PropTypes.string, PropTypes.func]),
--- packages/redux-routable-react/src/index.spec.js
+++ packages/redux-routable-react/src/index.spec.js
@@ -1,8 +1,9 @@
 import { createMemoryHistory } from 'history'
 import React from 'react'
+import { Provider } from 'react-redux'
 import TestRenderer from 'react-test-renderer'
 import configureStore from 'redux-mock-store'
-import { Route, Router, createMiddleware } from 'redux-routable'
+import { Route, Router, createMiddleware, routeChanged } from 'redux-routable'
 import { Link, RouterProvider } from './index'
 
 const mockRouter = Router([
@@ -16,28 +17,32 @@ const mocks = ({ router = mockRouter } = {}) => {
   const store = configureStore([middleware])()
   const create = children =>
     TestRenderer.create(
-      <RouterProvider router={router}>{children}</RouterProvider>,
+      <Provider store={store}>
+        <RouterProvider router={router}>{children}</RouterProvider>
+      </Provider>,
     ).toJSON()
+
+  window.open = jest.fn()
 
   return { store, history, window, create }
 }
 
 describe('Link', () => {
-  test('renders an <a> element by default', () => {
+  test('renders <a> element by default', () => {
     const { create } = mocks()
     const link = create(<Link route="home" />)
 
     expect(link.type).toBe('a')
   })
 
-  test('renders with a custom component', () => {
+  test('renders custom component', () => {
     const { create } = mocks()
     const link = create(<Link component="div" route="home" />)
 
     expect(link.type).toBe('div')
   })
 
-  test('generates the correct href', () => {
+  test('generates correct href', () => {
     const { create } = mocks()
     const link = create(
       <Link
@@ -48,5 +53,33 @@ describe('Link', () => {
     )
 
     expect(link.props.href).toBe('/search/widgets?query=devices#items')
+  })
+
+  test('allows other props to pass through', () => {
+    const { create } = mocks()
+    const link = create(<Link route="home" extra="extra" />)
+
+    expect(link.props.extra).toBe('extra')
+  })
+
+  test('dispatches ROUTE_CHANGED action when clicked', () => {
+    const { store, create } = mocks()
+    const action = routeChanged('search', { category: 'widgets' }, '')
+    const link = create(
+      <Link route="search" params={{ category: 'widgets' }} />,
+    )
+
+    link.props.onClick()
+
+    expect(store.getActions()).toEqual([action])
+  })
+
+  test('calls window.open() when clicked', () => {
+    const { create, window } = mocks()
+    const link = create(<Link route="home" action="open" />)
+
+    link.props.onClick()
+
+    expect(window.open).toHaveBeenCalled()
   })
 })
